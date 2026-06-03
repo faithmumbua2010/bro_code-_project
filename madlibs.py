bc2b6bcddfabde435cd6efae4fd847a15befa489
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-03 19:15:51 +0300

    Add new folders and files

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Madlibs game 
+# word game where youn creatde a story by filling blanks with words 
+adjective1= input("Enter an adjective: ")
+noun1= input("Enter a noun: ")
+adjective2= input("Enter another adjective: ")
+verb1= input("Enter a verb: ")
+adjective3= input("Enter one more adjective: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1} ")
+print(f"{noun1} was {adjective2}and {verb1}") 
+print(f"I was {adjective3}!")
---
+++ operators.py
@@ -0,0 +1,25 @@
+friends = 5
+
+#friends = friends+ 1
+#friends = friends - 1
+# friends = friends * 2
+# friends = friends / 2
+# friends = friends ** 2
+
+x=3.14
+y=-4
+z=5
+# result= round(x)
+#result= abs(y)
+#result= pow(4, 3)
+#result= max(x,y,z)
+#result = min (x,y,z)
+print(result)
+
+import math 
+x=9
+# print(math.pi) // 3.142...
+# result =math.sqrt(x)
+#result = math.ceil(x)
+# result = math.floor(x) 
+print(result)
