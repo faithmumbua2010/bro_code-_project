cce55fb4f753d2f5ae826bfa535b79da089fefe1
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-07 15:19:01 +0300

    add recent work

---
+++ temperature_conversion.py
@@ -0,0 +1,10 @@
+unit = input("Is this temperature in Celcius or Fahrenheit(C/F): ")
+temp =float(input("Enter the temperature: "))
+
+if unit== "C":
+    pass
+elif unit== "F":
+    pass
+else:
+    print(f"{unit} is invalid unit of measurement")
+
