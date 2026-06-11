091cfb2d0eafd4aeea33e56cf5c38a675d4bc8de
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-11 22:44:38 +0300

    anything for the activity bar

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -2,7 +2,8 @@ unit = input("Is this temperature in Celcius or Fahrenheit(C/F): ")
 temp =float(input("Enter the temperature: "))
 
 if unit== "C":
-    pass
+    temp= round((9 * temp)/5 +32, 1)
+    print(f"The temperature in fahrenheit is: {temp} F")
 elif unit== "F":
     pass
 else:
