1f90d5fb35fb807523c25a1a6c8c4cb447d13dcf
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-12 08:58:37 +0300

    logical operators and other exercises

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -5,7 +5,7 @@ if unit== "C":
     temp= round((9 * temp)/5 +32, 1)
     print(f"The temperature in fahrenheit is: {temp} F")
 elif unit== "F":
-    pass
+    temp= round((temp-32) *5/9, 1)
 else:
     print(f"{unit} is invalid unit of measurement")
 
