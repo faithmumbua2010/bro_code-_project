eeb8f20968e49c214718d260fa74f0888cb78b90
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-12 10:41:30 +0300

    adding to the operators

--- and_operator.py
+++ and_operator.py
@@ -1,8 +1,18 @@
 # and operator
 
-temp =28
+temp = 20
 is_sunny= True
 
 if temp>=28 and is_sunny:
     print("It is HOT outside ♨️ ")
     print("It is SUNNY ☀️")
+elif temp <=0 and is_sunny:
+    print("It is COLD outside 🥶")
+    print("It is SUNNY ☀️")
+
+elif 28> temp >0 and is_sunny:
+    print("It is WARM outside ☺️")
+    print("It is SUNNY ☀️")
+
+
+
---
+++ not_operator.py
@@ -0,0 +1,1 @@
+# It inverts the condition
