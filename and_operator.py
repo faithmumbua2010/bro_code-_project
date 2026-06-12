bcbd7b6f18898bafc5c08b8441c6d3c6a413b48b
Faith Mumbua <faithmumbua2010@gmail.com>
2026-06-12 10:28:02 +0300

    adding bugs😪

---
+++ and_operator.py
@@ -0,0 +1,8 @@
+# and operator
+
+temp =28
+is_sunny= True
+
+if temp>=28 and is_sunny:
+    print("It is HOT outside ♨️ ")
+    print("It is SUNNY ☀️")
--- logical_operators.py
+++ logical_operators.py
@@ -2,3 +2,13 @@
 #                    or= at least one condition must be True
 #                    and= both conditions must be True 
 #                    not = inverts the condition (not False, not True)
+
+# OR OPERATOR
+
+temp = 20
+is_raining= True
+
+if temp> 35 or temp<0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled")
